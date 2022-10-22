fa71eb2d7854fccd1072e53bc5dce99e5a578d65
Jaylen <me@jaylen.lol>
2022-10-22 15:11:40 -0400

    yas <3 love updating website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{tsx,ts,css}'],
   theme: {
     screens: {
       sm: "480px",
