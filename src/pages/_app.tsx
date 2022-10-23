6df4ab46e92e26e2b526ad5c2e08dd234f4896c2
Jaylen <me@jaylen.lol>
2022-10-23 18:20:36 -0400

    New changes to the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
+import { AppProps } from "next/app";
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
-
-export default MyApp
