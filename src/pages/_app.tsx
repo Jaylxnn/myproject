8796b95d98d22564421c5107c21ed4f9eddc0e41
Jaylen <me@jaylen.lol>
2022-11-03 00:47:35 -0400

    forgot this

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from "next/app";
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <div>
-      <Component {...pageProps} />
+    <div className="dark:text-white antialiased bg-ThemeDark bg-auto">
+      <div className="mx-auto max-w-4xl py-10 px-5">
+
+        <Component {...pageProps} />
+      </div>
     </div>
   );
 }
