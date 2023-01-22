27c07ae28dc3665c01e22481190ee82d4ef008bd
Jaylen <me@jaylen.lol>
2023-01-22 17:37:14 -0500

    testing dw

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log(req.body)
   res.status(200).json({ message: 'hey stinka butt! this feature of API isn\'t made yet, come back later...' })
 }
